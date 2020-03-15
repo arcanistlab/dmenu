6feed630111b40f4fc330f2bc2659ff669a996c8
Lauri Laks <laurilaks@protonmail.com>
2020-03-15 17:13:12 +0200

    line height patch

--- config.def.h
+++ config.def.h
@@ -4,17 +4,18 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"DejaVuSansMono:size=11"
+	"monospace:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#30302F" },
+	[SchemeNorm] = { "#c5c8c6", "#1D1F21" },
+	[SchemeSel] = { "#c5c8c6", "#30302F" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
